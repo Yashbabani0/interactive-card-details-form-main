f270781aaa7dd46ee3a28acd0b6b53a6f108afd3
Yash Babani <yashbabani09@gmail.com>
2024-05-06 14:49:54 +0530

    update

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ const viewCardName = document.querySelector('.cardholder_name')
 const viewCardDate = document.querySelector('.card_date')
 const viewCardMonth = document.querySelector('.card_month')
 const viewCardCvc = document.querySelector('.cvv')
+const complete = document.querySelector('.complete')
+const cardForm = document.querySelector('.cardForm')
+
+
+function setCardNumber (e) {
+  let value = e.target.value.replace(/\D/g, '');
+  value = value.replace(/(.{4})/g, "$1 ").trim();
+  viewCardNumber.innerText = value;
+}
+function setCardName (e) {
+  let value = e.target.value; 
+  viewCardName.innerText = value;
+}
+function setCardMonth (e) {
+  let value = e.target.value; 
+  viewCardDate.innerText = value;
+}
+function setCardYear  (e) {
+  let value = e.target.value;
+  viewCardMonth.innerText = value;
+}
+function setCVC (e) {
+  let value = e.target.value; 
+  viewCardCvc.innerText = value;
+}
+
+numberInput.addEventListener('input', setCardNumber);
+nameInput.addEventListener('input', setCardName);
+monthInput.addEventListener('input', setCardMonth);
+yearInput.addEventListener('input', setCardYear);
+cvcInput.addEventListener('input', setCVC);
